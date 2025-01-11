93774802f5c52dfa0047bafb077bce0e17836c85
IgnacioPena13 <ignacio.penasepulveda@outlook.com>
2025-01-11 20:53:32 +0100

    Enhance auth middleware with improved logging and token validation checks

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -5,19 +5,26 @@ module.exports = (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
 
-  if (authController.blacklist.has(token)) {
-    return res.status(400).json({ message: "Token has expired" });
-  }
+  console.log("Authorization Header:", authHeader);
+  console.log("Token:", token);
+
   if (!token) {
-    console.log("access denied.");
+    console.log("Access denied. No token provided.");
     return res.status(401).json({ message: "Access denied" });
+  }
+
+  if (authController.blacklist.has(token)) {
+    console.log("Token has expired.");
+    return res.status(400).json({ message: "Token has expired" });
   }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log("Decoded Token:", decoded);
     req.user = decoded;
     next();
   } catch (error) {
-    return res.status(403).json({ message: "invalid token" });
+    console.error("Invalid token:", error);
+    return res.status(403).json({ message: "Invalid token" });
   }
 };
